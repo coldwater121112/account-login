8bc4d5627684cd5b186b654887c8208d134b5484
coldwater121112 <coldwater121112@gmail.com>
2023-06-01 17:56:56 +0800

    feat: add user seeder

---
+++ models/seeds/accountLoginSeeder.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose")
+const accountLogin = require('../accountLogin') // 載入shortenURL model
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+
+const users = [
+  {
+    firstName: 'Tony',
+    email: 'tony@stark.com',
+    password: 'iamironman'
+  },
+  {
+    firstName: 'Steve',
+    email: 'captain@hotmail.com',
+    password: 'icandothisallday'
+  },
+  {
+    firstName: 'Peter',
+    email: 'peter@parker.com',
+    password: 'enajyram'
+  },
+  {
+    firstName: 'Natasha',
+    email: 'natasha@gamil.com',
+    password: '*parol#@$!'
+  },
+  {
+    firstName: 'Nick',
+    email: 'nick@shield.com',
+    password: 'password'
+  }
+]
+
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+  for (let i in users) {
+    accountLogin.create(users[i])
+  }
+  
+  console.log('done')
+})
