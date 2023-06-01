7c15f7e8b9ddcc9e074044c03d0cfdd4a3e05f1a
coldwater121112 <coldwater121112@gmail.com>
2023-06-02 01:56:54 +0800

    feat: add login post route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const mongoose = require("mongoose")
+const accountLogin = require("./models/accountLogin")
 const app = express()
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
@@ -29,6 +30,37 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   res.render('index')
 })
+
+// 處理 POST 請求，處理登入資料
+app.post('/', (req, res) => {
+  // 在這裡處理登入邏輯，例如驗證帳號密碼等等
+  const email = req.body.email
+  const password = req.body.password
+  console.log(email, password)
+  // 執行登入邏輯，進行帳號密碼驗證等等
+  accountLogin.findOne({ email: email })
+    .lean()
+    .then(user => {
+      if (!user) {
+        console.log('使用者不存在')
+        return res.redirect('/')
+      }
+      // 檢查密碼是否正確
+      if (user.password !== password) {
+        console.log('密碼錯誤')
+        return res.redirect('/')
+      }
+      else {
+        console.log('User not found')
+        res.status(404).send('User not found')
+      }
+    })
+    .catch(error => {
+      console.error(error)
+      res.status(500).send('Internal Server Error')
+    })
+})
+
 
 app.listen(3000, () => {
   console.log('App is runnung on port http://localhost:3000')
