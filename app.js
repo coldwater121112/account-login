41f510ef249d345355203d3ac45a792ed6bea57f
coldwater121112 <coldwater121112@gmail.com>
2023-06-02 15:50:28 +0800

    feat: add authentic login state​

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const port = process.env.PORT || 3000
 const bodyParser = require('body-parser')
 const mongoose = require("mongoose")
+const session = require("express-session")
 const accountLogin = require("./models/accountLogin")
 const app = express()
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
@@ -27,17 +29,24 @@ app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 
 
+// 設定 express-session
+app.use(session({
+  secret: 'your-secret-key', // 替換為一個安全的密鑰
+  resave: false,
+  saveUninitialized: true,
+}))
+
 app.get('/', (req, res) => {
+  console.log(req.session)
+  console.log(req.sessionID)
   res.render('index')
 })
 
 // 處理 POST 請求，處理登入資料
 app.post('/', (req, res) => {
-  // 在這裡處理登入邏輯，例如驗證帳號密碼等等
   const email = req.body.email
   const password = req.body.password
-  console.log(email, password)
-  // 執行登入邏輯，進行帳號密碼驗證等等
+
   accountLogin.findOne({ email: email })
     .lean()
     .then(user => {
@@ -45,14 +54,16 @@ app.post('/', (req, res) => {
         console.log('使用者不存在')
         return res.redirect('/')
       }
-      // 檢查密碼是否正確
+
       if (user.password !== password) {
         console.log('密碼錯誤')
         return res.redirect('/')
       }
-      else {
-        res.render('login', { username: user.firstName })
-      }
+
+      // 登入成功，將用戶存入 session
+      req.session.user = user.firstName
+      // console.log(req.session.user)
+      res.redirect('/login')
     })
     .catch(error => {
       console.error(error)
@@ -60,7 +71,18 @@ app.post('/', (req, res) => {
     })
 })
 
+// 驗證登入狀態
+app.get('/login', (req, res) => {
+  const userName = req.session.user
+  if (req.session.user) {
+    console.log('authenticated')
+    return res.render('login', { username: req.session.user })
+  } else {
+    console.log('not authenticated')
+    return res.redirect('/')
+  }
+})
 
-app.listen(3000, () => {
-  console.log('App is runnung on port http://localhost:3000')
+app.listen(port, () => {
+  console.log(`App is running on port http://localhost:${port}`)
 })
